1164c6b41c0cc9ca70c80b1d3f3dfa867dc3171c
Brian Burridge <bburridg@gmail.com>
2015-02-25 16:08:22 -0500

    Added seed file to create 100 fake movies.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,14 @@
 end
 
 User.where(name: "Guest", email: "guest@guest.com").first_or_create
+
+if Movie.count < 10
+  100.times do |counter|
+    random_movie_rating = Movie::RATINGS[rand(Movie::RATINGS.count)]
+    Movie.create(title: Faker::Name.title,
+                rating: random_movie_rating,
+                plot: Faker::Lorem.paragraph(rand(20)),
+                release_year: Faker::Time.between(25.years.ago, Time.now).year,
+                remote_movie_poster_url: "http://lorempixel.com/485/720/abstract/")
+  end
+end
