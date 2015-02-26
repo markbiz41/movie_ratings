a2dbfa2e1401e10fca13bf65785635561ba06af9
Brian Burridge <bburridg@gmail.com>
2015-02-26 09:31:28 -0500

    Added pagintion with kaminari and custom theme for bootstrap 3.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movie.page(params[:page]).per(15)
   end
 
   # GET /movies/1
