7b19cdce2d3b920e1dfc22a87a4b98373f744e3d
Brian Burridge <bburridg@gmail.com>
2015-03-01 22:49:40 -0500

    Added actual output to show page

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require 'carrierwave/orm/activerecord'
+#require 'carrierwave/orm/activerecord'
 
 module FirstApp
   class Application < Rails::Application
