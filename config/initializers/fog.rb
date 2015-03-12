a845614e74c48046c74d13f7e9797f1d52cd36fd
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-12 13:32:08 -0400

    added .idea to ignore file.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,9 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',                        # required
-      :aws_access_key_id      => ENV['S3_KEY'],                        # required
-      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
-      :endpoint               => 'http://s3.amazonaws.com' 
-  }
-  config.fog_directory = ENV['S3_BUCKET']
-end
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#       :provider               => 'AWS',                        # required
+#       :aws_access_key_id      => ENV['S3_KEY'],                        # required
+#       :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
+#       :endpoint               => 'http://s3.amazonaws.com'
+#   }
+#   config.fog_directory = ENV['S3_BUCKET']
+# end
