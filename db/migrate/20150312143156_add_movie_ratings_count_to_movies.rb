d374259211fa1d03a9ad3ffc12a29a588f23a4a0
Brian Burridge <bburridg@gmail.com>
2015-03-12 14:07:40 -0400

    Added alternative custom sql for updating counter cache.

--- db/migrate/20150312143156_add_movie_ratings_count_to_movies.rb
+++ db/migrate/20150312143156_add_movie_ratings_count_to_movies.rb
@@ -6,5 +6,12 @@ class AddMovieRatingsCountToMovies < ActiveRecord::Migration
     Movie.all.each do |m|
       Movie.reset_counters(m.id, :movie_ratings)
     end
+
+    # Alternatively, for better performance, this SQL can be used to update all the movies movie_ratings_counts
+    # ActiveRecord::Base.connection.execute("update movies
+    # set movie_ratings_count = counter_table.movie_ratings_counter
+    # from (select movie_id, count(*) as movie_ratings_counter
+    # from movie_ratings group by movie_id) as counter_table
+    # where counter_table.movie_id = movies.id;")
   end
 end
