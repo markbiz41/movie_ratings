19a15c7f8eed902fa0afaf82b69354b4e2b4a03a
Diana Davis <dianadryerdavis@yahoo.com>
2015-03-16 15:59:30 -0400

    adds routes for favorite, unfavorite

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,17 @@ Rails.application.routes.draw do
   get 'welcome/hello'
   #post 'welcome/hello'
   post 'welcome/hello' => 'welcome#hello', as: 'hello'
+
+
+
+
+  resources :movies do
+    member do
+      post 'favorite'
+      delete 'unfavorite'
+    end
+
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
